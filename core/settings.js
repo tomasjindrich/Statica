8a5d068690d93d6c1d9ab95c00ae006a2ffc709c
tomasjindrich <ahoj@tomasjindrich.cz>
2021-07-12 19:52:29 +0200

    hurón

--- core/settings.js
+++ core/settings.js
@@ -16,21 +16,21 @@ var settings_default = {
   },
 
   build: {
-    sitemap: true,
-    favicon: false,
-    img_resize: false,
+    sitemap    : true,
+    favicon    : false,
+    img_resize : false,
   },
 
   config: {
     
     env : process.env.NODE_ENV,
 
-    developer_name: 'Tomáš Jindřich',
-    developer_url: 'https://www.tomasjindrich.cz',
+    developer_name : 'Tomáš Jindřich',
+    developer_url  : 'https://www.tomasjindrich.cz',
 
     // host_dev: null pokud jde o verzi s relativními cestami
     // nebo vzdálená https://projekt.vyvojovy-server.cz
-    host_dev : null,   
+    host_dev : null,
     
     // host_prod: veřejná ostrá verze např https://www.projekt.cz,
     // přebírá se z site.url
@@ -45,7 +45,7 @@ var settings_default = {
       else return null;
      },
 
-    responsive_image_sizes: [500, 800, 1000, 1600, 2000, 2400],
+    responsive_image_sizes : [500, 800, 1000, 1600, 2000, 2400],
 
     file_path_before        : false,                // přidá před cesty k souborům, např wp-content/plugins/wpt/
     nice_urls               : false,
@@ -65,10 +65,11 @@ var settings_default = {
     name                     : '[default] Bramborářství Mrkvička',
     title                    : '[default] Prodej a výsadba bramborových věcí',
     description              : '[default] Popisek webu, několik vět o službě atd',
-    theme_color              : null,                                                 // Primární Theme barva
-    og_title                 : () => site('name'),                         // Open Graph titulek, defaultně se dědí ze site.name
-    og_description           : () => site('description'),                  // Open Graph popisek, defaultně se dědí ze site.description
-    og_site_name             : () => site('name'),                         // Open Graph site_name, defaultně se dědí ze site.name
+    theme_color_primary      : '#1c2536',
+    theme_color_secondary    : '#008800',
+    og_title                 : () => site('name'),                                   // Open Graph titulek, defaultně se dědí ze site.name
+    og_description           : () => site('description'),                            // Open Graph popisek, defaultně se dědí ze site.description
+    og_site_name             : () => site('name'),                                   // Open Graph site_name, defaultně se dědí ze site.name
     og_image_primary         : 'img/social/social-share-img-primary.jpg',            // Open Graph obrázek
     og_image_secondary       : 'img/social/social-share-img-secondary.jpg',          // Open Graph obrázek
     service_google_analytics : null,                                                 // Google Analytics ID
@@ -84,17 +85,17 @@ var settings_default = {
     //- používá se nejčastěji pro označení položky menu jako aktivní
     group : null,
     
-    title_prefix  : () => site('name'),      // Page Title se skládá z title_prefix ...
-    title_parser  : ' → ',                             // spojovník → / | -
+    title_prefix  : () => site('name'),    // Page Title se skládá z title_prefix ...
+    title_parser  : ' → ',                 // spojovník → / | -
     title_base    : () => site('title'),   // + title_base
-    title_postfix : null,                              // + title_postfix
+    title_postfix : null,                  // + title_postfix
     
     //- přednastavený formát title, dá se změnit v template-base
-    title_preset  : () => page('title_prefix') + page('title_parser') + page('title_base'), 
+    title_preset : () => page('title_prefix') + page('title_parser') + page('title_base'),
 
     description        : () => site('description'),          // Popisek konkrétní stránky
-    og_title           : () => site('og_title'),           // Open Graph titulek, defaultně se dědí ze site.og_title
-    og_description     : () => site('og_description'),     // Open Graph popisek, defaultně se dědí ze site.og_description
+    og_title           : () => site('og_title'),             // Open Graph titulek, defaultně se dědí ze site.og_title
+    og_description     : () => site('og_description'),       // Open Graph popisek, defaultně se dědí ze site.og_description
     og_image_primary   : () => site('og_image_primary'),     // Open Graph site_name, defaultně se dědí ze site.og_site_name
     og_image_secondary : () => site('og_image_secondary'),   // Open Graph site_name, defaultně se dědí ze site.og_site_name
   }
